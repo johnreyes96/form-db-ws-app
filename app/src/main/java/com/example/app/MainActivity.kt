6864e26fd53928b24374d5453950ebad72595635
johnfreyes <jhonfer96@hotmail.com>
2022-12-06 00:19:37 -0500

    remove menu toolbar

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -37,7 +37,6 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
