bd3df5218aaddc71573c9b647c4cad9f0d50869f
johnfreyes <jhonfer96@hotmail.com>
2022-12-08 17:31:25 -0500

    add mysql dependency and build connection with db and insert row

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,47 +1,96 @@
 package com.example.app
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.app.databinding.ActivityMainBinding
+import java.sql.*
+import java.util.Properties
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private var conn: Connection? = null
+    private var username = "root"
+    private var password = "admin"
+    private var ipWS = "192.168.1.57"
+    private var port = "3306"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
+        insertRow();
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             else -> super.onOptionsItemSelected(item)
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return super.onSupportNavigateUp()
+    private fun insertRow() {
+        val singer = findViewById<EditText>(R.id.txtSinger)
+        val sendButton = findViewById<Button>(R.id.btnSave)
+        sendButton.setOnClickListener {
+            if (executeQuery("${singer.text}")) {
+                Toast.makeText(this, "Cantante registrado", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Error al guardar el registro", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun executeQuery(value: String): Boolean {
+        var stmt: Statement? = null
+        getConnection()
+
+        try {
+            stmt = conn!!.createStatement()
+            return stmt.execute("INSERT INTO singer ('name') VALUES ('${value}');")
+        } catch (ex: SQLException) {
+            ex.printStackTrace()
+            return false
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close()
+                } catch (sqlEx: SQLException) {}
+                stmt = null
+            }
+            if (conn != null) {
+                try {
+                    conn!!.close()
+                } catch (sqlEx: SQLException) {}
+                conn = null
+            }
+        }
+    }
+
+    private fun getConnection() {
+        val connectionProps = Properties()
+        connectionProps["user"] = username
+        connectionProps["password"] = password
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance()
+            conn = DriverManager.getConnection("jdbc:mysql://$ipWS:$port/students", connectionProps)
+        } catch (ex: SQLException) {
+            ex.printStackTrace()
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
     }
 }
